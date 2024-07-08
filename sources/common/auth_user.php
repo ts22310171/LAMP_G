9840fe127291c07e6fe97444b140aab6253c92f5
orikooo3 <ori.kou.1025@gmail.com>
2024-07-08 14:42:29 +0900

    add: 認証機能の更新

---
+++ sources/common/auth_user.php
@@ -0,0 +1,39 @@
+<?php
+
+/********************************
+
+auth_member.php
+
+メンバーログイン認証
+認証が必要なページはこのファイルをインクルードする
+すでにlibs.phpがインクルードされている必要がある
+ *複数のサイトが同居またはユーザー管理と混同しないため
+$_SESSIONは多次元配列にする
+
+             2024/5/20 Y.YAMANOI
+ *********************************/
+session_start();
+if ((!isset($_SESSION['user']['user_name']))
+    || (!isset($_SESSION['user']['user_email']))
+) {
+    cutil::redirect_exit("login.php");
+}
+$user = new cuser();
+$row = $user->get_tgt_login(false, $_SESSION['user']['id']);
+if ($row === false || !isset($row['id'])) {
+    cutil::redirect_exit("login.php");
+}
+
+if ($row['id'] != $_SESSION['user']['id']) {
+    cutil::redirect_exit("login.php");
+}
+
+
+function get_member_name()
+{
+    if (isset($_SESSION['user']['name'])) {
+        return $_SESSION['user']['name'];
+    } else {
+        return '';
+    }
+}
