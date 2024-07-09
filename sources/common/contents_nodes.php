c992b83e24439581c7113f698d0a88dc90411d10
orikooo3 <ori.kou.1025@gmail.com>
2024-07-09 19:35:07 +0900

    add: ヘッダーとフッターを元に戻した

--- sources/common/contents_nodes.php
+++ sources/common/contents_nodes.php
@@ -45,58 +45,24 @@ class cheader extends cnode
 <!doctype html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-  <!-- Fonts -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-  <!-- Stylesheet -->
-  <link rel="stylesheet" href="http://wiz.developluna.jp/~d202425/LAMP_G/sources/css/app.css" />
-
-  <!-- Script -->
-  <script src="https://cdn.tailwindcss.com"></script>
-  <script src="tailwind.config.js"></script>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>PHPBase2サンプル</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-  <header>
-    <div class="bg-main h-20 flex justify-between items-center fixed top-0 left-0 w-full z-10 h-16">
-      <div class="flex items-center">
-        <a href="http://wiz.developluna.jp/~d202425/LAMP_G/sources/index.php" class="flex items-center mr-10">
-          <img src="http://wiz.developluna.jp/~d202425/LAMP_G/sources/images/GarbaGe favicon2.png" width="60" height="60" class="ml-4" />
-          <div class="-ml-2 text-2xl text-black font-bold">
-            Garba<span class="text-sub">Ge</span>
-          </div>
-        </a>
-        <a href="http://wiz.developluna.jp/~d202425/LAMP_G/sources/" class="pt-1 px-2">よくある質問</a>
-        <a href="http://wiz.developluna.jp/~d202425/LAMP_G/sources/" class="pt-1 px-2">お問い合わせ</a>
-      </div>
-      <div class="flex">
-        <?php if (isset(\$_SESSION['name'])): ?>
-          <div class="relative">
-            <div class="rounded shadow bg-whitecolor p-2">
-              <button class="text-blackcolor" id="btn">
-                <?php echo \$_SESSION['name']; ?>
-                <i class="fa-solid fa-chevron-down text-explain" id="arrow"></i>
-              </button>
-            </div>
-            <div class="dropdown hidden absolute right-0 mt-2 py-2 w-48 bg-white border rounded shadow-xl" id="dropdown">
-              <a href="" class="block px-4 py-2 text-sm text-explain" role="menuitem" tabindex="-1" id="menu-item-0"><i class="fa-solid fa-user"></i>マイページ</a>
-              <a href="" class="block px-4 py-2 text-sm text-explain" role="menuitem" tabindex="-1" id="menu-item-1"><i class="fa-solid fa-comment"></i>チャットルーム</a>
-              <a href="http://wiz.developluna.jp/~d202425/LAMP_G/sources/user/settigs.php" class="block px-4 py-2 text-sm text-explain" role="menuitem" tabindex="-1" id="menu-item-2"><i class="fa-solid fa-gear"></i>設定</a>
-              <a href="http://wiz.developluna.jp/~d202425/LAMP_G/sources/user/logout.php" class="block px-4 py-2 text-sm text-explain" role="menuitem" tabindex="-1" id="menu-item-2"><i class="fa-solid fa-gear"></i>ログアウト</a>
-            </div>
-          </div>
-        <?php else: ?>
-          <a href="http://wiz.developluna.jp/~d202425/LAMP_G/sources/user/login.php" class="text-black mr-5">
-            ログイン
-          </a>
-        <?php endif; ?>
-        <a href="http://wiz.developluna.jp/~d202425/LAMP_G/sources/"><i class="fa-solid fa-cart-shopping mx-5"></i></a>
-      </div>
-    </div>
-  </header>
-  <script src="http://wiz.developluna.jp/~d202425/LAMP_G/sources/js/header.js"></script>
+<!-- 全体コンテナ　-->
+<div class="container">
+<header class="d-flex justify-content-center py-3 border-dark border-bottom">
+<ul class="nav nav-pills">
+<li class="nav-item"><a href="index.php" class="nav-link link-success">メインメニュー</a></li>
+<li class="nav-item"><a href="prefecture_list.php" class="nav-link link-success">都道府県管理</a></li>
+<li class="nav-item"><a href="member_list.php" class="nav-link link-success">メンバー管理</a></li>
+<li class="nav-item"><a href="member_list_custom.php" class="nav-link link-success">メンバー管理（カスタムノード）</a></li>
+<li class="nav-item"><a href="hinagata.php" class="nav-link link-success">雛形ファイル</a></li>
+</ul>
+</header>
 END_BLOCK;
 		echo $echo_str;
 	}
@@ -461,48 +427,17 @@ class cfooter extends cnode
 	{
 		$echo_str = <<< END_BLOCK
 
-<footer class="bottom-0 w-full mt-auto">
-    <div class="bg-thingreen">
-      <div class="flex items-center">
-        <a href="http://wiz.developluna.jp/~d202425/LAMP_G/sources/index.php" class="flex items-center">
-          <img src="http://wiz.developluna.jp/~d202425/LAMP_G/sources/images/GarbaGe favicon3.png" width="60" height="60" class="ml-4" />
-          <h1 class="-ml-2 text-2xl font-bold text-blackcolor font-bold">
-            Garba<span class="text-sub">Ge</span>
-          </h1>
-        </a>
-      </div>
-      <div class="mx-auto w-full max-w-screen-xl flex-col p-4 md:flex md:items-center md:justify-between">
-        <ul class="mt-3 flex flex-wrap items-center text-base sm:mb-10 text-explain">
-          <li>
-            <a href="http://wiz.developluna.jp/~d202425/LAMP_G/sources/user/privacy.html" class="hover:underline"> プライバシーポリシー</a>
-          </li>
-          <li>
-          <li>
-            /
-          </li>
-          <a href="#" class="ml-1 hover:underline"> お問い合わせ</a>
-          </li>
-          <li>
-            /
-          </li>
-          <li>
-            <a href="#" class="ml-1 hover:underline">利用規約</a>
-          <li>
-            /
-          </li>
-          </li>
-          <li>
-            <a href="#" class="ml-1 hover:underline"> ヘルプ</a>
-          </li>
-        </ul>
-        <span class="text-xs text-explain sm:text-center">© 2024 GarbaGe Inc.
-        </span>
-      </div>
-    </div>
-  </footer>
-
+<footer class="py-3 my-4 border-dark border-top">
+<p class="text-center text-body-secondary">&copy; 2024 PHPBase2</p>
+</footer>
+</div>
+<!-- /全体コンテナ　-->
+<div class="b-divider"></div>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
 </body>
-
 </html>
 END_BLOCK;
 		echo $echo_str;
